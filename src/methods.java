fbf701845c48005001d0126cf355445bbd03ff28
Abir <abirmondal7864@gmail.com>
2025-07-09 00:29:35 +0530

    learnt about classes and objects

---
+++ src/methods.java
@@ -0,0 +1,22 @@
+class Computer{
+    public void playmusic(){
+        System.out.println("Music playing...");
+    }
+    public String getMeAPen(int cost){
+        if(cost>=10)
+            return "Pen fetched";
+        return "Nothing fetched";
+    }
+
+}
+
+public class methods {
+    public static void main(String[] a){
+    Computer comp=new Computer();
+    comp.playmusic();
+
+     int money=15;
+     String result=comp.getMeAPen(money);
+     System.out.println(result);
+    }
+}

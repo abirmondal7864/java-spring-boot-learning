55b4a0113aabff65036f3a158b9adaf0e224ad57
Abir <abirmondal7864@gmail.com>
2025-07-09 20:18:45 +0530

    inside package

---
+++ src/basics/Basics.java
@@ -0,0 +1,35 @@
+package basics;
+
+public class Basics {
+    public static void main(String[] args) {
+
+//        //Type casting
+//        int a =257;
+//        byte k =(byte)a; //Imp**
+//        //Explicitly type casting 'a' (int) to 'k' (byte)
+//        System.out.println(k);
+//        //Perform a modulo operation with the range of the byte (256): 257 % 256 = 1.
+//        //Therefore, the value stored in k will be 1.
+
+
+//        float f=5.6f;
+//        int t= (int)f;
+//        System.out.println(t);
+//        //the decimal part is truncated (chopped off). The value is not rounded.
+
+
+//        //Type promotion
+//        byte a=10;
+//        byte b=30;
+//        int result=a*b;
+//        System.out.println(result);
+
+
+//        // Type promotion and explicit cast
+//        char ch = 'a';
+//        char result = (char)(ch + 1); //Imp***
+//        // => (char) (97+1)=(char) 98 =b
+//        System.out.println(result); //b
+
+    }
+}

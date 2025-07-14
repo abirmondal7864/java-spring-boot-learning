c188615b2cc932326801b24e95dc68b5887eda12
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-07-14 20:13:14 +0530

    Array of objects

---
+++ src/arrays/arrayOfObjects.java
@@ -0,0 +1,36 @@
+package arrays;
+class Student {
+    int roll;
+    String name;
+    int age;
+
+}
+public class arrayOfObjects {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.roll=1;
+        s1.name="Abir";
+        s1.age=10;
+
+        Student s2=new Student();
+        s2.roll=2;
+        s2.name="Sounak";
+        s2.age=8;
+
+        Student s3=new Student();
+        s3.roll=3;
+        s3.name="Akash";
+        s3.age=12;
+
+        Student []students= new Student[3];//creating an array that can hold 3 references to Student objects.
+
+
+        students[0]=s1;
+        students[1]=s2;
+        students[2]=s3;
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("Name: "+ students[i].name+", Roll: "+ students[i].roll+", Age: "+ students[i].age);
+        }
+    }
+}

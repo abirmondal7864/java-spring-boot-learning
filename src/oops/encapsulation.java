6ad30b928a2211a5c41f015c548d22160a54763a
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-07-17 12:20:36 +0530

    Default vs Parameterized Constructor

--- src/oops/encapsulation.java
+++ src/oops/encapsulation.java
@@ -4,23 +4,32 @@ class Human {
     private int age = 12;
     private String name = "Abir";
 
-    public Human(){ //constructor (write no return type,same name as class name)**
-        //called everytime an object created
-        age=12;
+    //Constructor
+    // write no return type,same name as class name
+    //called automatically everytime an object created
+    public Human(){  //default constructor
+        age=13;
         name="John";
     }
-    public int getAge() {
+    public Human(String name){  //parameterized constructor
+        this.age=15;
+        this.name=name;
+    }
+    public Human(int age,String name){  //parameterized constructor
+        this.age=age;
+        this.name=name;
+    }
+
+
+    public int getAge() { //getter
         return age;
     }
-
-    public void setAge(int age) {
+    public void setAge(int age) { //setter
         this.age = age;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -29,8 +38,11 @@ class Human {
 public class encapsulation {
     public static void main(String[] args) {
         Human obj = new Human();
-        Human obj1=new Human();
         System.out.println(obj.getName() + " : " + obj.getAge());
+        Human obj1 = new Human("Akash");
+        System.out.println(obj1.getName() + " : " + obj1.getAge());
+        Human obj2=new Human(18,"Abir");
+        System.out.println(obj2.getName() + " : " + obj2.getAge());
 //        obj.setAge(12);
 //        obj.setName("Mondal");
 //        System.out.println(obj.getName() + " : " + obj.getAge());

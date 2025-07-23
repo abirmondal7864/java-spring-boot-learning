5bb49818e2fd26077684d41ce4fb9a2961c19b2b
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-07-23 10:47:25 +0530

    final keyword

---
+++ src/finalkeyword/FinalKeyword.java
@@ -0,0 +1,29 @@
+package finalkeyword;
+
+public class FinalKeyword {
+
+//1. Final Variable
+//    Once assigned, its value cannot be changed. It makes the variable a constant.
+    final int x = 10;
+    x = 20; // Error: cannot assign a value to final variable 'x'
+
+}
+
+//2. Final Method
+//Prevents the method from being overridden in a subclass.
+class A{
+    final void display(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    void display(){}// Error: cannot override final method
+}
+
+//3. Final Class
+//Prevents the class from being inherited (no subclassing).
+
+final class C{
+
+}
+class  D extends C{ }// Error: cannot inherit from final class

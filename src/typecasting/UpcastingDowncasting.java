c2a16b302c8b71198cd52e45900fb35a82d309dc
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-07-23 11:50:01 +0530

    Upcasting and Downcasting

---
+++ src/typecasting/UpcastingDowncasting.java
@@ -0,0 +1,24 @@
+package typecasting;
+
+
+class A{
+    void show1(){
+        System.out.println("in A show");
+    }
+}
+class B extends A{
+    void show2(){
+        System.out.println("in B show");
+    }
+}
+public class UpcastingDowncasting {
+    public static void main(String[] args) {
+        //Upcasting
+        A obj=new B();
+        obj.show1(); //in A show
+
+        //Downcasting
+        B obj1 = (B) obj;
+        obj1.show1(); //in A show
+    }
+}

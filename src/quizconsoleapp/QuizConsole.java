d3998112b88298826926c1d0788829622e0197c4
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-07-29 17:42:24 +0530

    Initial commit

---
+++ src/quizconsoleapp/QuizConsole.java
@@ -0,0 +1,9 @@
+package quizconsoleapp;
+
+public class QuizConsole {
+    public static void main(String[] args) {
+
+    QuestionService service=new QuestionService();
+    service.displayQuestions();
+    }
+}

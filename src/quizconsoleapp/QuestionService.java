38951f27edd191846de292a95943de7a5be5deea
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-07-29 20:32:09 +0530

    Play quiz commit

--- src/quizconsoleapp/QuestionService.java
+++ src/quizconsoleapp/QuestionService.java
@@ -1,7 +1,10 @@
 package quizconsoleapp;
 
+import java.util.Scanner;
+
 public class QuestionService {
     Question[] questions = new Question[5];
+    String [] selection=new String[5];
 
     //constructor
     public QuestionService() {
@@ -13,10 +16,22 @@ public class QuestionService {
     }
 
     public void displayQuestions() {
+        int i=0;
         for (Question q : questions) {
-            System.out.println(q);
+            System.out.println("Question no.: "+q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+            Scanner sc=new Scanner(System.in);
+            selection[i]=sc.nextLine();
+            i++;
         }
 
+        for(String s:selection){
+            System.out.println(s);
+        }
 
     }
 }

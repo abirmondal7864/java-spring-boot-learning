9f93529d46d645ce76834d1d87ded17a21291fa1
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-08-05 12:54:48 +0530

    Add practice file about Introduction to Interface

---
+++ src/oops/Interface/Interface.java
@@ -0,0 +1,51 @@
+package oops.Interface;
+
+/*
+  Interface A demonstrates a basic Java interface with two abstract methods
+  and two constants (implicitly public, static, and final).
+ */
+interface A {
+    // Constants (public, static, and final by default in interfaces)
+    int age = 23;
+    String city = "Kalyani";
+
+    // Methods (public and abstract by default) //must be overridden
+    void show();
+    void config();
+}
+
+/*
+  Class B implements interface A and provides definitions for its abstract methods.
+ */
+class B implements A {
+
+    // Implementing the 'show' method
+    @Override
+    public void show() {
+        System.out.println("In show");
+    }
+
+    // Implementing the 'config' method
+    @Override
+    public void config() {
+        System.out.println("In config");
+    }
+}
+
+/*
+  Main class to demonstrate the use of interface and implementation.
+ */
+public class Interface {
+    public static void main(String[] args) {
+        // Declare interface reference and instantiate with implementing class
+        A obj = new B();
+
+        // Call implemented methods
+        obj.show();
+        obj.config();
+
+        // Access interface constants
+        System.out.println(A.age);
+        System.out.println(A.city);
+    }
+}
